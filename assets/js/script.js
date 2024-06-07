de45516d0628c89d88c62933a7643acd77943190
BrenoHenriqueRR <breno.reis@aluno.ifsp.edu.br>
2024-06-07 15:44:42 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+document.getElementById('open_btn').addEventListener('click', function(){
+    document.getElementById('menu-lat').classList.toggle('open-menu');
+    document.getElementById('overlay').classList.toggle('active');
+    document.getElementById('body').classList.toggle('no-scroll');
+})
+document.getElementById('open_btn2').addEventListener('click', function(){
+    document.getElementById('menu-lat').classList.toggle('open-menu');
+    document.getElementById('overlay').classList.toggle('active');
+    document.getElementById('body').classList.toggle('no-scroll');
+})
+
