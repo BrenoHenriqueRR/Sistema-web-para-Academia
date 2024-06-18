1a7f5b373d1ea6572c1f3b260f57f28bb4c7a163
BrenoHenriqueRR <breno.reis@aluno.ifsp.edu.br>
2024-06-18 19:09:20 -0300

    ultimo-commit

--- assets/js/funcoes.js
+++ assets/js/funcoes.js
@@ -1,27 +1,55 @@
-function exibirForm(){
+function exibirForm() {
     var form = document.getElementById("form-promocao");
     var valor = '' // variavel para armazenar os dados do formulário
     var codpromo = Math.ceil(Math.random() * 1000); // numeros inteiros de 0 a 1000
     var formData = new FormData(form); // cria um conjunto de chaves e valor ,como um array: [ key : valor ]
     for (var valores of formData.entries()) {
-        valor+= `${valores[0]}: ${valores[1]}\n`; // atribui os valores do form a variavel valor
+        valor += `${valores[0]}: ${valores[1]}\n`; // atribui os valores do form a variavel valor
         console.log(valores[0] + ': ' + valores[1]); // exibe no console
     }
     alert(valor + '\nCodigo promocional: ' + codpromo) // Exibe na tela os valores do cadastro mais o seu codigo promocional
     //aqui guardaria essas informações no banco possivelmente
 }
 
-function cpf(v){ //replace permite substituir paartes de uma string
-    v=v.replace(/\D/g,"")    // /D = carcteres que nao são digitos,/g = modificador global                
-    v=v.replace(/(\d{3})(\d)/,"$1.$2")      // \d{3} captura os 3 primeiros digitos $1.$2 adiciona um ponto entre os numero capturados
-    v=v.replace(/(\d{3})(\d)/,"$1.$2")  //mesma ideia acima    
-                                            
-    v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2") // captura os dois digitos ao final da string e coloca um hífen antes deles
+function cpf(v) { //replace permite substituir paartes de uma string
+    v = v.replace(/\D/g, "")    // /D = carcteres que nao são digitos,/g = modificador global                
+    v = v.replace(/(\d{3})(\d)/, "$1.$2")      // \d{3} captura os 3 primeiros digitos $1.$2 adiciona um ponto entre os numero capturados
+    v = v.replace(/(\d{3})(\d)/, "$1.$2")  //mesma ideia acima    
+
+    v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2") // captura os dois digitos ao final da string e coloca um hífen antes deles
     return v
 }
 
 function getQueryParam(param) {
-    let params = new URLSearchParams(window.location.search); 
+    let params = new URLSearchParams(window.location.search);
     return params.get(param); // pega os parametros da url
+}
+
+function exibircomprovante() {
+    var formcad = document.getElementById("formcad");
+    var codigoP = document.getElementById("cod").value;
+    let valor = '';
+
+    if (formcad) {
+        let formdata = new FormData(formcad);
+        for (var valores of formdata.entries()) {
+            valor += `${valores[0]}: ${valores[1]}\n`;
+            console.log(valores[0] + ': ' + valores[1]);
+        }
+        addEventListener('submit', function (event) {
+            event.preventDefault(); // evitar o refresh na pagina quando houver um submit
+            event.target.reset(); // resetar os forms
+        });
+
+        if (codigoP == '') {
+            alert(valor + '\nTotal = R$ 79,80');
+        }else{
+            alert(valor + "Codigo Promocional: " + codigoP + '\nTotal = R$ 69,80');
+        }
+
+        location.href = '../../index.html';
+    }
+
+    // Adiciona a nova div ao final do contêiner
 }
 
