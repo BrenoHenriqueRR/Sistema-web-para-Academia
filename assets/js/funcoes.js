4ae6c541e1d65b1ef05c67ecc701207fe3d46570
BrenoHenriqueRR <breno.reis@aluno.ifsp.edu.br>
2024-06-18 08:32:08 -0300

    cad

--- assets/js/funcoes.js
+++ assets/js/funcoes.js
@@ -1,11 +1,12 @@
 function exibirForm(){
     var form = document.getElementById("form-promocao");
     var valor = '' // variavel para armazenar os dados do formulário
+    var codpromo = Math.ceil(Math.random() * 1000); // numeros inteiros de 0 a 1000
     var formData = new FormData(form);
     for (var valores of formData.entries()) {
         valor+= `${valores[0]}: ${valores[1]}\n`;
         console.log(valores[0] + ': ' + valores[1]);
     }
-    alert(valor);
+    alert(valor + '\nCodigo promocional: ' + codpromo)
 }
 
